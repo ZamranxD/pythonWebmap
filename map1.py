50d2f0bd06e7867a23ceffabc3f0f57717f63268
ZamranxD <zamran.butt.1@gmail.com>
2020-07-18 19:40:48 +0500

    adding markers for volcanoes in US

--- map1.py
+++ map1.py
@@ -1,4 +1,12 @@
 import folium
+import pandas
+
+
+#processing the Volcanoes' data file to get their longitude and latitude so that we can use them later to make markers
+volcanic_data = pandas.read_csv("Volcanoes.txt")
+latitude = list(volcanic_data["LAT"]) #extracting the latitude column and converting it into a list
+longitude = list(volcanic_data["LON"]) #extracting the longitude column and converting it into a list
+
 
 #creating an instance of the Map object provided by folium
 #and since map is a reserved keyword, so i'll have to use another variable name
@@ -6,8 +14,11 @@ map1 = folium.Map(location=[30.375321, 69.345116], zoom_start=6, tiles="Stamen T
 
 #making a feature group to add more layers/children to the map instance
 fg = folium.FeatureGroup(name="layer1")
-#making a marker and adding it on top of the base layer
-fg.add_child(folium.Marker(location=[30.375321, 69.345116], popup="Welcome to Pakistan!", icon=folium.Icon(color="blue")))
+#making multiple markers for the volcanoes by looping through the latitude and longitude lists
+for lat, lon in zip(latitude, longitude):
+    fg.add_child(folium.Marker(location=[lat, lon], popup="This is a volcano!", icon=folium.Icon(color="red")))
+
+
 map1.add_child(fg)
 
 #converting this map object to an html file so that it can be viewed in the browser
