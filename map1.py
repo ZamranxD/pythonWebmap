45f623636ca64f770264e4e8614ce77be9a5efb9
ZamranxD <zamran.butt.1@gmail.com>
2020-07-19 09:14:43 +0500

    added a layer control feature

--- map1.py
+++ map1.py
@@ -21,18 +21,23 @@ def color_prod(elevation):
 map1 = folium.Map(location=[38.375321, -99.345116], zoom_start=6, tiles="Stamen Terrain")
 
 #making a feature group to add more layers/children to the map instance
-fg = folium.FeatureGroup(name="layer1")
+fgv = folium.FeatureGroup(name="Volcano Markers")
 #making multiple markers for the volcanoes by looping through the latitude and longitude lists
 #also making the popups dynamic by pushing the elevation values in them
 for lat, lon, elev in zip(latitude, longitude, elevation):
-    fg.add_child(folium.CircleMarker(location=[lat, lon], radius=6, popup=str(elev)+"m", fill_color = color_prod(elev), color = 'grey', fill_opacity = 0.7))
+    fgv.add_child(folium.CircleMarker(location=[lat, lon], radius=6, popup=str(elev)+"m",
+     fill_color = color_prod(elev), color = 'grey', fill_opacity = 0.7))
 
 #color grading the countries according to the polulation
-fg.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
+#making a feature group for layer control
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
 style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'} ))
 
-map1.add_child(fg)
+map1.add_child(fgv)
+map1.add_child(fgp)
+map1.add_child(folium.LayerControl()) #this layer control helps to turn layers on and off from the UI
 
 #converting this map object to an html file so that it can be viewed in the browser
 map1.save("map1.html")
